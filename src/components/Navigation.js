376b582fc863474e365808024c160c61fe867e5c
sebich <seb082011@gmail.com>
2021-06-04 19:35:39 +0200

    Mise en place du component navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <div className="sidebar">
+      <div className="id">
+        <img src="./media/sebastien-fernandes.png" alt="profil-pic" />
+        <h3>Sébastien Fernandes</h3>
+      </div>
+
+      <div className="navigation">
+        <ul>
+          <li>
+            <NavLink exact to="/" activeClassName="navActive">
+              <i className="fas fa-home"></i>
+              <span>Accueil</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/competences" activeClassName="navActive">
+              <i className="fass fa fa-laptop-code"></i>
+              <span>Compétences</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/portfolio" activeClassName="navActive">
+              <i className="fas fa-images"></i>
+              <span>Portfolio</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/contact" activeClassName="navActive">
+              <i className="fas fa-address-card"></i>
+              <span>Contact</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="socialNetwork">
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sebastien-fernandes-7a7169165/"
+              // title= bonne pratique Opquast
+              title="nouvelle fenêtre profil linkedin"
+              // noopener interdit toutes modifications de l'URL
+              // noreferrer empêche les sites externes visés par le lien d'obtenir des information sur l'origine du trafic
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/sebich"
+              title="nouvelle fenêtre profil github"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
+
+        <div className="signature">
+          <p>Sébastien fernandes - 2021</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navigation from '../components/Navigation';
 
 const home = () => {
-  return <div>Home c'est ici !</div>;
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
 };
 
 export default home;
